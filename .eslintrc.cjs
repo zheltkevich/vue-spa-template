fac82136daba9857ac4c42458051d8b51037fca2
Aleksandr <zheltkevich42@gmail.com>
2024-02-03 13:49:54 +0300

    Config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,13 +2,37 @@
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
-  root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    root: true,
+    env: { node: true },
+    extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', '@vue/eslint-config-prettier/skip-formatting'],
+    parserOptions: { ecmaVersion: 'latest' },
+    overrides: [
+        {
+            files: ['*.html', '*.vue'],
+            processor: 'vue/.vue',
+        },
+    ],
+    rules: {
+        'quotes': ['error', 'single'],
+        'no-console': [
+            'error',
+            {
+                allow: ['warn', 'error'],
+            },
+        ],
+        'no-unused-vars': [
+            'error',
+            {
+                argsIgnorePattern: '^_',
+            },
+        ],
+        'vue/attribute-hyphenation': ['warn', 'never'],
+        'vue/v-on-event-hyphenation': [
+            'error',
+            'never',
+            {
+                autofix: true,
+            },
+        ],
+    },
 }
