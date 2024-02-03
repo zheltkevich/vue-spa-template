885f01df2101f805cac9ea5604d477cdfd24184a
Aleksandr <zheltkevich42@gmail.com>
2024-02-03 16:38:03 +0300

    test

--- src/App.vue
+++ src/App.vue
@@ -11,10 +11,8 @@ import HelloWorld from './components/HelloWorld.vue'
             src="@/assets/logo.svg"
             width="125"
             height="125" />
-
         <div class="wrapper">
             <HelloWorld msg="You did it!" />
-
             <nav>
                 <RouterLink to="/">Home</RouterLink>
                 <RouterLink to="/about">About</RouterLink>
