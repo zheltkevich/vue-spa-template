f98bd04c9be4235e92deee9122c92e3b92eef46b
Aleksandr <zheltkevich42@gmail.com>
2024-02-03 13:24:31 +0300

    Init

---
+++ .prettierrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+    $schema: 'https://json.schemastore.org/prettierrc',
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: false,
+    singleAttributePerLine: true,
+    endOfLine: 'lf',
+    vueIndentScriptAndStyle: false,
+    proseWrap: 'preserve',
+    arrowParens: 'avoid',
+    bracketSameLine: true,
+    bracketSpacing: true,
+    quoteProps: 'consistent',
+    singleQuote: true,
+    printWidth: 150,
+}
