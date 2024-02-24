777591d8954bc24c1e28ecb9b9f24490dd671036
Aleksandr <zheltkevich42@gmail.com>
2024-02-24 12:22:52 +0300

    Update config

---
+++ src/components/base/BaseButton.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+
+<template>
+    <button class="base-button">
+        <span
+            v-if="$slots.prepend"
+            class="base-button__slot base-button__slot--prepend">
+            <slot name="prepend" />
+        </span>
+        <span
+            v-if="$slots.default"
+            class="base-button__slot base-button__slot--default">
+            <slot>Button</slot>
+        </span>
+        <span
+            v-if="$slots.append"
+            class="base-button__slot base-button__slot--append">
+            <slot name="append" />
+        </span>
+    </button>
+</template>
--- src/components/blocks/AppNavigation.vue
+++ src/components/blocks/AppNavigation.vue
@@ -1,8 +1,13 @@
+<script setup>
+import BaseButton from '@base/BaseButton.vue'
+</script>
+
 <template>
     <nav class="nav">
         <nav>
             <RouterLink :to="{ name: 'home' }">Home page</RouterLink>
             <RouterLink :to="{ name: 'another' }">Another page</RouterLink>
+            <BaseButton>Button</BaseButton>
         </nav>
     </nav>
 </template>
